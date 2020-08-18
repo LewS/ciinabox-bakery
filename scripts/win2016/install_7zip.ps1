ac6f1a4e6b2a49a9c31385dfaa37663798751a46
Lew <lew@redgrid.net>
2020-08-18 20:06:01 +1000

    add system

--- scripts/win2016/install_7zip.ps1
+++ scripts/win2016/install_7zip.ps1
@@ -21,7 +21,7 @@ try
 
   # Download 7zip
   Write-Output "Downloading 7zip from $7zip_download_url"
-  [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+  [System.Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
   $wc = new-object System.Net.WebClient
   $wc.DownloadFile($7zip_download_url,"$temp_folder\" + $7zip_download_url.Split("/")[-1])
 
