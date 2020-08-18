d7b647efce79e0c420eda614e2d4a5ce103bc1da
Lew <lew@redgrid.net>
2020-08-19 09:21:58 +1000

    correct url path

--- scripts/win2016/install_7zip.ps1
+++ scripts/win2016/install_7zip.ps1
@@ -7,7 +7,7 @@ try
   $start_time = Get-Date
 
   # Set download URLs
-  $7zip_download_url = "https://downloads.sourceforge.net/sevenzip/7za465.zip"
+  $7zip_download_url = "https://downloads.sourceforge.net/project/sevenzip/7-Zip/4.65/7za465.zip"
 
   # Create Software folder
   $software_folder = "$env:SystemDrive\software"
